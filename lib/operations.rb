4e0911e2ea35eb37bb3aa063cf66e0be005db46e
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 03:25:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else (speed >= 40 || speed <= 60)
+   return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	true ? (speed > 60) || (speed < 40) : speed == (40...60)
 end
 	
 
